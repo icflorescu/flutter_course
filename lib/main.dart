c0bc9bef43833fa1325a8327adf9c789ccd161bd
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-04-19 22:52:09 +0300

    Finished chapter 10

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,12 @@ class _MyAppState extends State<MyApp> {
     print(_products);
   }
 
+  void _updateProduct(int index, Map<String, dynamic> product) {
+    setState(() {
+      _products[index] = product;
+    });
+  }
+
   void _deleteProduct(int index) {
     setState(() {
       _products.removeAt(index);
@@ -50,8 +56,8 @@ class _MyAppState extends State<MyApp> {
       routes: {
         '/': (BuildContext context) => AuthPage(),
         '/products': (BuildContext context) => ProductsPage(_products),
-        '/admin': (BuildContext context) =>
-            ProductsAdminPage(_addProduct, _deleteProduct),
+        '/admin': (BuildContext context) => ProductsAdminPage(
+            _addProduct, _updateProduct, _deleteProduct, _products),
       },
       onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,8 +1,63 @@
 import 'package:flutter/material.dart';
 
+import './product_edit.dart';
+
 class ProductListPage extends StatelessWidget {
+  final List<Map<String, dynamic>> products;
+  final Function updateProduct;
+  final Function deleteProduct;
+
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
+
+  Widget _buildEditButton(BuildContext context, int index) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext context) {
+            return ProductEditPage(
+              product: products[index],
+              updateProduct: updateProduct,
+              productIndex: index,
+            );
+          }),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('All Products'));
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(color: Colors.red),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            } else if (direction == DismissDirection.startToEnd) {
+              print('Swiped start to end');
+            } else {
+              print('Other type of swiping');
+            }
+          },
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image']),
+                ),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price'].toString()}'),
+                trailing: _buildEditButton(context, index),
+              ),
+              Divider()
+            ],
+          ),
+        );
+      },
+      itemCount: products.length,
+    );
   }
 }
